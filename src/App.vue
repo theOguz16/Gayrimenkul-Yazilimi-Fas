4e3551e0694688a4b23aa91a0d4777dc9edd9cb4
theOguz16 <oguzhanuyar531@gmail.com>
2024-08-26 15:45:28 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="container"></div>
+  <RouterView />
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <Header></Header>
+  <div
+    class="container mt-6 flex gap-8 items-start justify-center flex-row max-sm:p-4 max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:text-center max-lg:flex-col"
+  >
+    Selams
+  </div>
+</template>
